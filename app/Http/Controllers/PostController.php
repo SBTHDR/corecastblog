5450679cd39433f2701749ce3b8b8305066bb793
SBTHDR <subrata.halder13d@gmail.com>
2021-09-02 13:15:07 +0600

    Post Controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::latest();
+
+        if (request('search')) {
+            $posts->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%');
+        }
+
+        return view('posts', [
+            'posts' => $posts->get(),
+            'categories' => Category::all()
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('post', [
+            'post' => $post
+        ]);
+    }
+}
