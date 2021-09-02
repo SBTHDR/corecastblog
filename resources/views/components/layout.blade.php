39b565eec4fd66f7eb094cc9601146f7040c9cf6
SBTHDR <subrata.halder13d@gmail.com>
2021-09-03 02:29:03 +0600

    session flash messsage with alpine.js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -54,4 +54,11 @@
         </div>
     </footer>
 </section>
+
+    @if(session()->has('success'))
+        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 2000)" x-show="show" class="inline-block p-3 fixed bg-blue-500 bottom-3 right-3 rounded-xl text-white">
+            <p>{{ session('success') }}</p>
+        </div>
+    @endif
+
 </body>
