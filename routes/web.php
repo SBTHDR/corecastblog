0f8204000838cc4f96ef3cd8b0e6daded2b589fd
SBTHDR <subrata.halder13d@gmail.com>
2021-09-02 13:05:51 +0600

    Implement Search

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    $posts = Post::latest();
+
+    if (request('search')) {
+        $posts->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%');
+    }
+
     return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get(),
+        'posts' => $posts->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('post/{post:slug}', function (Post $post) {
     return view('post', [ 'post' => $post ]);
