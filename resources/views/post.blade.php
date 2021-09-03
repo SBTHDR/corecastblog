16b5f5d39c48f7975b644b617b78e313ff42ae78
SBTHDR <subrata.halder13d@gmail.com>
2021-09-03 14:52:51 +0600

    Comments component migrations and factory

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -48,6 +48,16 @@
                     </p>
                 </div>
             </div>
+
+            <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                <h1 class="text-sm font-bold">Comments</h1>
+
+                <x-post-comment />
+                <x-post-comment />
+                <x-post-comment />
+                <x-post-comment />
+            </section>
+
         </article>
     </main>
 </x-layout>
